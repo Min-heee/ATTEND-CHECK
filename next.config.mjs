543af3a710f4ce7238e415743d2f8a641217ffb9
Byung hee Min <byungheemin@Byungui-MacBookPro.local>
2024-10-15 21:38:40 +0900

    Disable ESLint during builds

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-};
+    eslint: {
+      ignoreDuringBuilds: true, // 빌드 시 ESLint 무시
+    },
+  };
+  
+  export default nextConfig;
 
-export default nextConfig;
+  
